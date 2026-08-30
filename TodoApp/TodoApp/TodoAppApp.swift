53bf6dfaa6225fcb335621bee65f691a927a47c7
Xulun Huang <xulunh01@gmail.com>
2026-08-30 09:32:13 -0400

    Add iOS app with local networking configured

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Xulun Huang on 8/30/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
